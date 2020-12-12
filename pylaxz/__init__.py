fa58b4d33d627fde21ebd54f9deae6f72d66ba0b
minlaxz <minminlaxz@gmail.com>
2020-12-13 02:27:13 +0630

    Formatting code.

--- pylaxz/__init__.py
+++ pylaxz/__init__.py
@@ -1,11 +1,11 @@
 """
 pylaxz
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
 Copyright (c) 2020 Min Latt.
 License: MIT, see LICENSE for more details.
 """
-# populate fields for >>>help(imagezmq)
 
-from .__meta__ import *
+# populate fields for >>> help(pylaxz)
+from .__meta__ import __name__, __description__, __url__, __version__
+from .__meta__ import __author__, __author_email__, __license__
 from .utils import *
