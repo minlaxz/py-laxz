e5435fb840197d18cecdcaef5bbb1d1d1829ba38
minlaxz <minminlaxz@gmail.com>
2020-12-13 15:21:00 +0630

    Setting up as  python moudule.
    python -m pylaxz <args>

--- pylaxz/__main__.py
+++ pylaxz/__main__.py
@@ -1,16 +1,28 @@
 import sys
 from .utils.logxs import *
 
+helper = """
+python -m pylaxz help
+"""
+
+
 def main(**kwargs) -> None:
     for k, v in kwargs.items():
-        printf('{} = {}'.format(k,v), internal=True)
+        printf('{} = {}'.format(k, v), _int=True)
+
+
+def main_():
+    printf(helper, _int=True)
+
 
 if __name__ == "__main__":
-    try:
-        main ( ** dict(arg.split('=') for arg in sys.argv[1:]))
+    main_() if 'help' in sys.argv else printf(helper, _int=True)
+        
+    # try:
+    #     main ( ** dict(arg.split('=') for arg in sys.argv[1:]))
 
-    except ValueError:
-        printf('Internal Error', internal=True)
-    
-    except Exception as e:
-        printf(e, internal=True)
+    # except ValueError:
+    #     printf('Internal Error', internal=True)
+
+    # except Exception as e:
+    #     printf(e, internal=True)
