69e6f8cd401a352a9d896c3af659e3ddd5c08fd7
minlaxz <minminlaxz@gmail.com>
2020-12-13 22:17:09 +0630

    Direct invoke TODO.

--- pylaxz/__main__.py
+++ pylaxz/__main__.py
@@ -1,4 +1,6 @@
-import sys
+from subprocess import run as _run
+from subprocess import PIPE as _PIPE
+from sys import argv as _argv
 from .utils.logxs import *
 
 helper = """
@@ -11,12 +13,20 @@ def main(**kwargs) -> None:
         printf('{} = {}'.format(k, v), _int=True)
 
 
-def main_():
-    printf(helper, _int=True)
+def main_help():
+    printf('I am helping you.', _int=True)
 
+def main_updater():
+    cmd = "find . -maxdepth 2 -name '*.py' -print0 | xargs -0 sha1sum | sort -h | sha256sum | awk '{print $1}'"
+    result = _run(cmd, shell=True, check=False, stdout=_PIPE, universal_newlines=True)
+    printf(result.stdout[:-1], _int=True)
 
 if __name__ == "__main__":
-    main_() if 'help' in sys.argv else printf(helper, _int=True)
+    if len(_argv) == 1:
+        printf(helper, _int=True)
+    else:
+        if 'help' in _argv: main_help()
+        elif 'update' in _argv: main_updater()
         
     # try:
     #     main ( ** dict(arg.split('=') for arg in sys.argv[1:]))
